81509bfa93551900f079cfa93e7ca1888f3b2c13
aneesh <aneeshpatil341@gmail.com>
2023-08-19 09:57:31 +0530

    if logged in reder page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import Page from './Page'
 
 function Login() {
+    const[isLoggedIn,setIsLoggedIn]=useState(false)
     const[formData, seFormData]=useState({
         username:'',
         password:''
@@ -13,7 +15,18 @@ function Login() {
         e.preventDefault();
         fetch(`http://localhost:4040/login?`+ new URLSearchParams(formData))
         .then(res => res.json())
-        .then(dt => console.log('LOGIN DATA FROM API', dt))
+        // .then(dt => {
+        //     console.log('LOGIN DATA FROM API', dt)
+        //     return dt
+        // })
+        .then(dt => {
+            console.log('LOGIN DATA FROM API', dt);
+            // Assuming your API returns a successful message message
+            if (dt?.message === 'Login successful') {
+                console.log('LOGIN ', dt);
+                setIsLoggedIn(true); // Set login status to true
+            }
+        })
         .catch(er => console.log('ERR', er))
        
        
@@ -22,11 +35,11 @@ function Login() {
 
     return (
         <div className='login'>
-            <form onSubmit={handleSubmit} className='form-width'>
+            { isLoggedIn ? (<Page/>) : (<form onSubmit={handleSubmit} className='form-width'>
                 <input type='text' name='username' value={formData.username} onChange={handleChange}/>username
                 <input type='password' name='password' value={formData.password} onChange={handleChange}/>password
                 <button type='submit' className="btn btn-success">Login</button>
-            </form>
+            </form>)}
         </div>
     )
 }
